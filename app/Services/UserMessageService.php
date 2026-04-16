e4d9ceb9748a5db15943cf61dcbb7cb1ff2084fe
kyam-wvs <kyam@wvs.org.uk>
2026-04-16 07:33:48 +0100

    init user message page

---
+++ app/Services/UserMessageService.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+class UserMessageService
+{
+    public function messageUsers(int $files): array
+    {
+
+    }
+}
--- resources/views/pages/user/⚡message.blade.php
+++ resources/views/pages/user/⚡message.blade.php
@@ -1,13 +1,34 @@
 <?php
 
 use Livewire\Component;
+use App\Services\CsvService;
 
 new class extends Component
 {
-    //
+    public function save()
+    {
+    }
 };
 ?>
 
 <div>
-    <p>TODO</p>
+    <h2>CSV File upload</h2>
+    <form wire:submit="save">
+        <label>
+            Users to message
+            <input type="number" wire:model="rows">
+        </label>
+        <form wire:submit="save">
+
+        <button type="submit">Send messages</button>
+        </form>
+    </form>
+
+    <h3>Jobs</h3>
+    <ul>
+        @foreach ($jobs as $job)
+            <li>Job {{ $job['jobNumber'] }}: {{ $job['timeTaken'] }} seconds</li>
+        @endforeach
+    </ul>
+    <p>Total time taken: {{ $timeTakenMs }} ms</p>
 </div>
